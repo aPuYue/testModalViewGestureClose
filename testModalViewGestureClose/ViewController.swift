b592761b9d508d078d3216c334c1d400c238fc91
aPuYue <pu.yue@access-company.com>
2021-05-14 18:01:47 +0900

    First commit

---
+++ testModalViewGestureClose/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testModalViewGestureClose
+//
+//  Created by puyue on R 3/05/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
